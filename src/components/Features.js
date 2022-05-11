0f7be3fad4b5bf4990396eee63bfce393f836a20
Anan Wang <mengan.wang@gmail.com>
2022-05-11 02:11:28 -0700

    added brothers pictures

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,18 +3,24 @@ import PropTypes from "prop-types";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
+  <div className="columns is-multiline" 
+  style = {{
+    maxWidth: "1200px",
+    margin: "0 auto",
+
+  }}>
     {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
+      <div key={item.text} className="column is-3">
         <section className="section">
           <div className="has-text-centered">
             <div
               style={{
-                width: "240px",
+                width: "120px",
                 display: "inline-block",
               }}
             >
               <PreviewCompatibleImage imageInfo={item} />
+              <p>Hello</p>
             </div>
           </div>
           <p>{item.text}</p>
