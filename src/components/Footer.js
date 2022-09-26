36f11488ac80ee6baea064e5ab3085ef640c775a
rudyorre <rudyorre@gmail.com>
2022-09-25 22:15:30 -0700

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,124 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
 import vimeo from "../img/social/vimeo.svg";
 
-const Footer = class extends React.Component {
+import logo from "../img/logo.png";
+
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <div class="bg-charcoal" id="footer" style={{ paddingTop: '15px', backgroundColor: '#1F1F1F', color: 'white' }}>
+      <div class="container">
+        <div class="row pv5">
+          <div class="col-xs-12 col-sm-3" style={{ marginRight: '15px'}}>
+            <div class="flex flex-row items-center mb3">
+              <a href="/chairs"><img class="" src={logo}/></a>
+              <div class="header pale-gray">Theta Tau</div>
+            </div>
+            <p>Upsilon Delta Chapter</p>
+            <p>University of California, Los Angeles</p>
+          </div>
+          <div class="col-xs-6 col-sm-2 mt3" style={{paddingTop: '15px', textDecoration: 'none'}}>
+            <h3 class="mb3 gray">General</h3>
+            <p class="pale-gray"><a href="http://thetatau.org/">National Theta Tau</a></p>
+            <p class="pale-gray"><a href="/values">About</a></p>
+            {/*<p class="pale-gray"><a href="/chairs">Chairs</a></p>*/}
+          </div>
+          <div class="col-xs-6 col-sm-2 mt3" style={{paddingTop: '15px'}}>
+            <h3 class="mb3 gray">Recruitment</h3>
+            <p><a href="/rush">Information</a></p>
+            {/*<p><a href="/rush_faq">FAQ</a></p>*/}
+            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSd38J8pHiHZtMR4eufrqfy9C5khLJmmr5UzMgY2j6aj_S1-3Q/viewform">Apply</a></p>
+          </div>
+          <div class="col-xs-6 col-sm-3 mt3" style={{paddingTop: '15px'}}>
+            <h3 class="mb3 gray">Contact</h3>
+            {/*<p class="pale-gray reverse"></p>*/}
+            <p><a href="https://www.facebook.com/ThetaTauUCLA/">Facebook</a></p>
+            <p><a href="https://www.instagram.com/uclathetatau/">Instagram</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Footer2 = () => {
+  return (
+    <footer>
+      <Box>
+        <Container>
+          <Row>
+            <Column>
+              <Heading>About Us</Heading>
+              <FooterLink href="#">Aim</FooterLink>
+              <FooterLink href="#">Vision</FooterLink>
+              <FooterLink href="#">Testimonials</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Services</Heading>
+              <FooterLink href="#">Writing</FooterLink>
+              <FooterLink href="#">Internships</FooterLink>
+              <FooterLink href="#">Coding</FooterLink>
+              <FooterLink href="#">Teaching</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Contact Us</Heading>
+              <FooterLink href="#">Uttar Pradesh</FooterLink>
+              <FooterLink href="#">Ahemdabad</FooterLink>
+              <FooterLink href="#">Indore</FooterLink>
+              <FooterLink href="#">Mumbai</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Social Media</Heading>
+              <FooterLink href="#">
+                <i className="fab fa-facebook-f">
+                  <span style={{ marginLeft: "10px" }}>
+                    Facebook
+                  </span>
+                </i>
+              </FooterLink>
+              <FooterLink href="#">
+                <i className="fab fa-instagram">
+                  <span style={{ marginLeft: "10px" }}>
+                    Instagram
+                  </span>
+                </i>
+              </FooterLink>
+              <FooterLink href="#">
+                <i className="fab fa-twitter">
+                  <span style={{ marginLeft: "10px" }}>
+                    Twitter
+                  </span>
+                </i>
+              </FooterLink>
+              <FooterLink href="#">
+                <i className="fab fa-youtube">
+                  <span style={{ marginLeft: "10px" }}>
+                    Youtube
+                  </span>
+                </i>
+              </FooterLink>
+            </Column>
+          </Row>
+        </Container>
+      </Box>
+    </footer>
+  );
+}
+
+const FooterOld = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
