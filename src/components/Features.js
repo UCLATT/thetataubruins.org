9d92971b01ee4cb6865e7c7ae3598e23e8948364
Alan Vuong <alankvuong@gmail.com>
2024-11-26 01:48:17 -0800

    Optimizing layout of actives, leadership, and alumni page to account for differences in image height on desktop versus mobile

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,46 +3,81 @@ import PropTypes from "prop-types";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import "./styles.css";
 
-const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline grid-columns" 
-  style = {{
-    width: "100%",
-    maxWidth: "1200px",
-    margin: "0 auto",
-  }}>
-    {gridItems.map((item) => (
-      <div key={item.text} className="column is-3">
-        <section className="section" style={{
-          
-        }}>
-          <div>
+const FeatureGrid = ({ gridItems }) => {
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    // Function to check screen width and set `isMobile` accordingly
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    // Check on component mount
+    handleResize();
+
+    // Add event listener to update on resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div
+      className="columns is-multiline grid-columns"
+      style={{
+        width: "100%",
+        maxWidth: "1200px",
+        margin: "0 auto",
+      }}
+    >
+      {gridItems.map((item) => (
+        <div key={item.text} className="column is-3">
+          <section
+            className="section"
+            style={{
+              minHeight: isMobile ? "200px" : "280px",
+            }}
+          >
             <div
               style={{
+                minHeight: isMobile ? "200px" : "280px",
                 width: "100%",
-                minHeight: "280px",
-                justifyContent: "center",
-                display: "grid",
               }}
             >
-              <PreviewCompatibleImage imageInfo={item}/>
+              <div
+                style={{
+                  width: "100%",
+                  justifyContent: "center",
+                  display: "grid",
+                }}
+              >
+                <PreviewCompatibleImage imageInfo={item} />
+              </div>
             </div>
-          </div>
-          <div style = {{
-            textAlign: "center",
-          }}>
-            <p style={{
-              fontWeight: "bold",
-              marginTop: "5px",
-              fontSize: "1.2rem",
-            }}>{item.text}</p>
-            <p>{item.major}</p>
-            <p>{item.year}</p>
-          </div>
-        </section>
-      </div>
-    ))}
-  </div>
-);
+            <div
+              style={{
+                textAlign: "center",
+              }}
+            >
+              <p
+                style={{
+                  fontWeight: "bold",
+                  marginTop: "5px",
+                  fontSize: "1.2rem",
+                }}
+              >
+                {item.text}
+              </p>
+              <p>{item.major}</p>
+              <p>{item.year}</p>
+            </div>
+          </section>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 FeatureGrid.propTypes = {
   gridItems: PropTypes.arrayOf(
